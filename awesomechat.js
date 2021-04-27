749a4de9ebc825796c615733861647e84dc50376
QuynhPT <quynhpham@edutalk.edu.vn>
2021-04-27 09:59:27 +0700

    done

--- awesomechat.js
+++ awesomechat.js
@@ -1,48 +1,94 @@
+// var express = require('express'),
+//     app = express(),
+//     server = require('http').createServer(app),
+
+//     io = require('socket.io')(server);
+
+//     // io = require('socket.io').listen(server),
+//     users = {};
+//     server.listen(8888);
+//     app.use(express.static(__dirname + '/public'));
+//     app.get('/', function(req, res){
+//     res.sendfile(__dirname + '/views/chatui.html');
+//     });
+
+//     io.sockets.on('connection', function(socket){
+//     socket.on('new user', function(name, data){
+//             if (name in users){
+//                 data(false);
+//             } else {
+//                 data(true);
+//             socket.nickname = name;
+//             users[socket.nickname] = socket;
+//             console.log('add nickName');
+//             updateNickNames();
+//         }
+
+//     });
+
+//     function updateNickNames(){ 
+//         io.sockets.emit('usernames', Object.keys(users));
+//     }
+//     socket.on('open-chatbox', function(data){
+//         users[data].emit('openbox', {nick: socket.nickname});
+//     });
+//     socket.on('send message',function(data, sendto){
+//         users[sendto].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
+//         users[socket.nickname].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
+
+//         console.log(data);
+//     });
+//     socket.on('disconnect', function(data){
+//         if (!socket.nickname) return;
+//         delete users[socket.nickname];
+//         updateNickNames();
+//     });
+//     });
+
 var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io')(server),
+	users = {};
+server.listen(process.env.PORT || 5000);
 
-    io = require('socket.io')(server);
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/views/chatui.html');
+});
 
-    // io = require('socket.io').listen(server),
-    users = {};
-    server.listen(8888);
-    app.use(express.static(__dirname + '/public'));
-    app.get('/', function(req, res){
-    res.sendfile(__dirname + '/views/chatui.html');
-    });
+io.sockets.on('connection', function(socket){
+	socket.on('new user', function(name, data){
+			if (name in users){
+				data(false);
+			}else{
+				data(true);
+			socket.nickname = name;
+			users[socket.nickname] = socket;
+			console.log('add nickName');
+			updateNickNames();
+		}
 
-    io.sockets.on('connection', function(socket){
-    socket.on('new user', function(name, data){
-            if (name in users){
-                data(false);
-            } else {
-                data(true);
-            socket.nickname = name;
-            users[socket.nickname] = socket;
-            console.log('add nickName');
-            updateNickNames();
-        }
+	});
 
-    });
+	function updateNickNames(){
+		io.sockets.emit('usernames', Object.keys(users));
+	}
+	socket.on('open-chatbox', function(data){
+		users[data].emit('openbox', {nick: socket.nickname});
+	});
+	socket.on('send message',function(data, sendto){
+		users[sendto].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
+		users[socket.nickname].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
 
-    function updateNickNames(){ 
-        io.sockets.emit('usernames', Object.keys(users));
-    }
-    socket.on('open-chatbox', function(data){
-        users[data].emit('openbox', {nick: socket.nickname});
-    });
-    socket.on('send message',function(data, sendto){
-        users[sendto].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
-        users[socket.nickname].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
+		console.log(data);
+	});
+	socket.on('disconnect', function(data){
+		if (!socket.nickname) return;
+		delete users[socket.nickname];
+		updateNickNames();
+	});
+});
 
-        console.log(data);
-    });
-    socket.on('disconnect', function(data){
-        if (!socket.nickname) return;
-        delete users[socket.nickname];
-        updateNickNames();
-    });
-    });
 
     module.exports = app;
